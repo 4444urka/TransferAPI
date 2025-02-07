7ec55b4a55ab221ddc88215d10103bab4944260c
amnyam11 <mishinandrey851@gmail.com>
2025-02-08 01:47:59 +1000

    added user registration view

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -1,1 +1,8 @@
+from .serializers import UserRegistrationSerializer
+from rest_framework import generics
+from .models import User
 
+
+class RegistrationUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer

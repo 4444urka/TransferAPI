6b8c7c375dbe276447dcdbaf580657789b31e092
amnyam11 <mishinandrey851@gmail.com>
2025-02-08 02:22:37 +1000

    added view for access token

--- src/apps/auth/views.py
+++ src/apps/auth/views.py
@@ -1,8 +1,12 @@
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, MyTokenObtainPairSerializer
 from rest_framework import generics
 from .models import User
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 class RegistrationUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer

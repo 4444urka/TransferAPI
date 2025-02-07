0640056307a6286e77ab2fc9d7f33a39839ba2fd
amnyam11 <mishinandrey851@gmail.com>
2025-02-08 02:23:49 +1000

    added routes for take tokens

--- src/apps/auth/urls.py
+++ src/apps/auth/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RegistrationUserView
+from .views import RegistrationUserView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('register/', RegistrationUserView.as_view(), name='user-register')
+    path('register/', RegistrationUserView.as_view(), name='user-register'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

4b0b411e049a0849951d5d73d8edec5657fc4746
amnyam11 <mishinandrey851@gmail.com>
2025-02-08 01:45:49 +1000

    added user registration serializer

--- src/apps/auth/serializers.py
+++ src/apps/auth/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
-from apps.auth.models import User
+from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['phone_number', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            phone_number=validated_data['phone_number'],
+            password=validated_data['password']
+        )
+        return user
 

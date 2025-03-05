fad36da56bd1c740ecd3ba0d672617c8538d2f15
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-05 23:16:52 +1000

    refactor booking datetime field and enhance user creation logging

--- src/apps/booking/models.py
+++ src/apps/booking/models.py
@@ -12,7 +12,7 @@ class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     trip = models.ForeignKey(Trip, on_delete=models.DO_NOTHING, default=1)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.DO_NOTHING, default=1)
-    booking_datetime = models.DateTimeField(default=timezone.now())
+    booking_datetime = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
 
     seats = models.ManyToManyField(Seat, blank=True)

cd6a7c7430bbe284e2b5bb162c446cdc88925347
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-05 19:04:18 +1000

    register Booking model in admin and update Booking fields

--- src/apps/booking/models.py
+++ src/apps/booking/models.py
@@ -1,6 +1,9 @@
+from django.utils import timezone
+
 from django.db import models
 
 from apps.auth.models import User
+from apps.seat.models import Seat
 from apps.trip.models import Trip
 from apps.vehicle.models import Vehicle
 
@@ -9,10 +12,13 @@ class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     trip = models.ForeignKey(Trip, on_delete=models.DO_NOTHING, default=1)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.DO_NOTHING, default=1)
-    # TODO: Добавить связзи с другими моделями (и создать их)
-    booking_datetime = models.DateTimeField()
+    booking_datetime = models.DateTimeField(default=timezone.now())
+    is_active = models.BooleanField(default=True)
 
+    seats = models.ManyToManyField(Seat, blank=True)
 
+    def __str__(self):
+        return f"{self.user} - {self.trip} - {self.booking_datetime}"
 
 
 

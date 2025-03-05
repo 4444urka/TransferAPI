26305426f651d7ed76a42182e16394a7793d742c
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-05 19:04:32 +1000

    update Trip model to use time fields for departure and arrival

--- src/apps/trip/models.py
+++ src/apps/trip/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Trip(models.Model):
     origin = models.CharField(max_length=30)
     destination = models.CharField(max_length=30)
-    departure_datetime = models.DateTimeField()
-    arrival_datetime = models.DateTimeField()
+    departure_time = models.TimeField(default='00:00')
+    arrival_time = models.TimeField(default='00:00')
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.origin} - {self.destination}"
+        return f"{self.departure_time}: {self.origin} - {self.destination}"

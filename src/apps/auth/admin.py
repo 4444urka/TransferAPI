2d6596909e915afbeda19cf90da53200b62ed936
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-07 10:45:54 +1000

    customize admin interface for User model with custom forms and fields

--- src/apps/auth/admin.py
+++ src/apps/auth/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms
 from apps.auth.models import User
 
-# Register your models here.
-admin.site.register(User)
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('phone_number', 'first_name', 'last_name', 'password')
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('phone_number', 'first_name', 'last_name', 'password')
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    fieldsets = (
+        (None, {'fields': ('password',)}),
+        ('Personal info', {'fields': ('phone_number', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('id','phone_number', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('phone_number', 'first_name', 'last_name')
+    ordering = ('phone_number',)
+
+admin.site.register(User, CustomUserAdmin)

64e0c9881499e2072dab0f790ec647acd349fc5a
Denis <terehenkokiki@gmail.com>
2025-03-08 15:19:20 +1000

    сделал валидации trips

--- src/apps/trip/models.py
+++ src/apps/trip/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
+from datetime import datetime, timedelta
 
 from apps.vehicle.models import Vehicle
 
@@ -16,3 +18,41 @@ class Trip(models.Model):
 
     def __str__(self):
         return f"{self.date} {self.departure_time}: {self.origin} - {self.destination}"
+
+    def clean(self):
+        super().clean()
+        
+        # Проверка что время отправления раньше времени прибытия
+        if self.departure_time >= self.arrival_time:
+            raise ValidationError("Время отправления должно быть раньше времени прибытия")
+
+        # Проверка пересечения с другими поездками того же транспорта
+        overlapping_trips = Trip.objects.filter(
+            vehicle=self.vehicle,
+            date=self.date
+        ).exclude(pk=self.pk)
+
+        for trip in overlapping_trips:
+            # Проверяем пересечение времени
+            if (self.departure_time <= trip.arrival_time and 
+                self.arrival_time >= trip.departure_time):
+                raise ValidationError(
+                    f"Обнаружено пересечение с поездкой {trip}. " +
+                    "Транспортное средство уже занято в это время."
+                )
+            
+            # # Проверка минимального интервала между поездками (30 минут) (если понадобится)
+            # min_interval = timedelta(minutes=30)
+            
+            # # Конвертируем время в datetime для сравнения
+            # trip_arrival_dt = datetime.combine(self.date, trip.arrival_time)
+            # next_departure_dt = datetime.combine(self.date, self.departure_time)
+            
+            # if abs(next_departure_dt - trip_arrival_dt) < min_interval:
+            #     raise ValidationError(
+            #         "Минимальный интервал между поездками должен быть 30 минут"
+            #     )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)

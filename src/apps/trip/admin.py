b25946373412e0c3e70f22a37415988fb5c618aa
amnyam11 <mishinandrey851@gmail.com>
2025-03-09 13:37:38 +1000

    reworked trip validation, moved into model

--- src/apps/trip/admin.py
+++ src/apps/trip/admin.py
@@ -1,53 +1,51 @@
-# admin.py
-from django import forms
 from django.contrib import admin
-from django.utils import timezone
-from django.db.models import Q
+from django import forms
 from .models import Trip
-
-class TripForm(forms.ModelForm):
-    class Meta:
-        model = Trip
-        fields = '__all__'
-
-    def clean(self):
-        cleaned_data = super().clean()
-        departure = cleaned_data.get('departure_time')
-        arrival = cleaned_data.get('arrival_time')
-        vehicle = cleaned_data.get('vehicle')
-
-        if not all([departure, arrival, vehicle]):
-            return cleaned_data
-
-        # Валидация времени
-        current_time = timezone.now()
-        if departure < current_time:
-            raise forms.ValidationError("Нельзя создавать поездки в прошлом")
-
-        if arrival <= departure:
-            raise forms.ValidationError("Время прибытия должно быть позже отправления")
-
-        # Проверка доступности транспорта
-        conflicts = Trip.objects.filter(vehicle=vehicle).filter(
-            Q(departure_time__lt=arrival, arrival_time__gt=departure)
-        ).exclude(pk=self.instance.pk if self.instance else None)
-
-        if conflicts.exists():
-            raise forms.ValidationError(
-                f"Транспорт {vehicle} занят с {conflicts[0].departure_time} до {conflicts[0].arrival_time}"
-            )
-
-        return cleaned_data
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
-    form = TripForm
-    list_display = ('origin', 'destination', 'formatted_departure', 'vehicle', 'default_ticket_price')
+    list_display = (
+        'origin', 
+        'destination',
+        'formatted_departure',
+        'formatted_arrival',
+        'vehicle',
+        'default_ticket_price'
+    )
     list_filter = ('vehicle', 'departure_time')
     search_fields = ('origin', 'destination')
+    date_hierarchy = 'departure_time'
+    ordering = ('-departure_time',)
 
-    # данные о времени в админке будут отображаться в UTC, 
-    # но при редактировании или создании в UTC+10 (учитывая timezone)
+    # Кастомные методы для форматирования времени (нужно для отображения времени в формате UTC) 
+    # - хз надо это или нет, пока пусть будет
     def formatted_departure(self, obj):
         return obj.departure_time.strftime('%Y-%m-%d %H:%M')
     formatted_departure.short_description = 'Departure Time'
+
+    def formatted_arrival(self, obj):
+        return obj.arrival_time.strftime('%Y-%m-%d %H:%M')
+    formatted_arrival.short_description = 'Arrival Time'
+
+    # Настройка формы редактирования
+    fieldsets = (
+        ('Основная информация', {
+            'fields': (
+                'origin', 
+                'destination',
+                'vehicle',
+                'departure_time',
+                'arrival_time',
+                'default_ticket_price'
+            ),
+            'description': '<div class="help">Все времена указываются в часовом поясе сервера (UTC+10)</div>'
+        }),
+    )
+
+    # Валидация при сохранении
+    def save_model(self, request, obj, form, change):
+        try:
+            obj.full_clean()  # Активируем валидацию модели
+            super().save_model(request, obj, form, change)
+        except forms.ValidationError as e:
+            form.add_error(None, e)  # Показываем ошибки в форме
--- src/apps/trip/models.py
+++ src/apps/trip/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils import timezone
-
 from apps.vehicle.models import Vehicle
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 
 class Trip(models.Model):
@@ -16,6 +17,44 @@ class Trip(models.Model):
         )
 
     default_ticket_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    # время в формате UTC
+
     def __str__(self):
         return f"{self.departure_time.strftime('%Y-%m-%d %H:%M')}: {self.origin} - {self.destination}"
+    
+    def clean(self):
+        """Универсальная валидация для всех способов сохранения"""
+        # Проверка времени отправления
+        if self.departure_time < timezone.now():
+            raise ValidationError({
+                'departure_time': 'Нельзя создавать поездки с прошедшей датой отправления'
+            })
+
+        # Проверка времени прибытия
+        if self.arrival_time <= self.departure_time:
+            raise ValidationError({
+                'arrival_time': 'Время прибытия должно быть позже отправления'
+            })
+
+        # Проверка пересечения временных интервалов
+        conflicts = Trip.objects.filter(
+            Q(vehicle=self.vehicle),
+            Q(departure_time__lt=self.arrival_time),
+            Q(arrival_time__gt=self.departure_time)
+        ).exclude(pk=self.pk if self.pk else None)
+
+        if conflicts.exists():
+            raise ValidationError({
+                'vehicle': f'Транспорт занят с {conflicts[0].departure_time} до {conflicts[0].arrival_time}'
+            })
+
+        # Проверка цены
+        if self.default_ticket_price < 0:
+            raise ValidationError({
+                'default_ticket_price': 'Цена не может быть отрицательной'
+            })
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    

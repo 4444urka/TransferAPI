980c72d6da30f7c8db9a5570d0d0bd9afdb0abd6
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-10 09:51:27 +1000

    Add CitySerializer and enhance TripDetailSerializer with available seats calculation

--- src/apps/trip/serializers.py
+++ src/apps/trip/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from .models import Trip
+from .models import Trip, City
+from apps.vehicle.serializers import VehicleSerializer
 
-class TripSerializer(serializers.ModelSerializer):
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ['id', 'name']
+
+
+class TripDetailSerializer(serializers.ModelSerializer):
+    origin = CitySerializer(read_only=True)
+    destination = CitySerializer(read_only=True)
+    vehicle = VehicleSerializer(read_only=True)
+    available_seats = serializers.SerializerMethodField()
+
     class Meta:
         model = Trip
-        fields = '__all__'
+        fields = ['id', 'origin', 'destination', 'departure_time', 'arrival_time',
+                  'vehicle', 'default_ticket_price', 'available_seats']
 
-    # в сериализаторах будем валидировать только данные специфичные для api
+    def get_available_seats(self, obj):
+        """Возвращает количество свободных мест"""
+        from apps.seat.models import Seat
+        return Seat.objects.filter(vehicle=obj.vehicle, is_booked=False).count()

b63d546dc41fb1a08bc99fb383c8f7eb38889e03
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-14 13:07:39 +1000

    Enhance VehicleViewSet with Swagger documentation for vehicle operations

--- src/apps/vehicle/views.py
+++ src/apps/vehicle/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -36,6 +37,61 @@ class VehicleViewSet(viewsets.ModelViewSet):
     ordering = ['-created_at']
     pagination_class = None
 
+
+    @swagger_auto_schema(
+        operation_description="Получение списка всех транспортных средств.",
+        operation_summary="Список транспортных средств",
+        tags=["Транспортные средства"]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+
+    @swagger_auto_schema(
+        operation_description="Получение информации о конкретном транспортном средстве.",
+        operation_summary="Детали транспортного средства",
+        tags=["Транспортные средства"]
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+
+    @swagger_auto_schema(
+        operation_description="Создание нового транспортного средства. Доступно только администраторам.",
+        operation_summary="Создание транспортного средства",
+        tags=["Транспортные средства"]
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+
+    @swagger_auto_schema(
+        operation_description="Обновление информации о транспортном средстве. Доступно только администраторам.",
+        operation_summary="Обновление транспортного средства",
+        tags=["Транспортные средства"]
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+
+    @swagger_auto_schema(
+        operation_description="Частичное обновление информации о транспортном средстве. Доступно только администраторам.",
+        operation_summary="Частичное обновление транспортного средства",
+        tags=["Транспортные средства"]
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+
+    @swagger_auto_schema(
+        operation_description="Удаление транспортного средства. Доступно только администраторам.",
+        operation_summary="Удаление транспортного средства",
+        tags=["Транспортные средства"]
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+
     def get_queryset(self):
         queryset = super().get_queryset()
         search = self.request.query_params.get('search', None)
@@ -48,6 +104,11 @@ class VehicleViewSet(viewsets.ModelViewSet):
             return VehicleDetailSerializer
         return VehicleSerializer
 
+    @swagger_auto_schema(
+        operation_description="Проверка доступности транспортного средства..",
+        operation_summary="Доступность транспортного средства",
+        tags=["Транспортные средства"]
+    )
     @action(detail=True, methods=['get'])
     def availability(self, request, pk=None):
         """

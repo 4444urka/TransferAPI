50e8677be7ccc36c997fded3e0e1015665e08f8b
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-14 13:07:13 +1000

    Add MyTokenRefreshView and Swagger documentation for user authentication endpoints

--- src/apps/auth/urls.py
+++ src/apps/auth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegistrationUserView, MyTokenObtainPairView
+from .views import RegistrationUserView, MyTokenObtainPairView, MyTokenRefreshView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegistrationUserView.as_view(), name='user-register'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
 ]

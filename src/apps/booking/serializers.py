ee99c5f73dd37ac5b1f643df2edcf9570a5179d7
4444urka <149653447+4444urka@users.noreply.github.com>
2025-03-25 10:04:21 +1000

    Now you need to send seat_numbers not ids and better loging

--- src/apps/booking/serializers.py
+++ src/apps/booking/serializers.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework import serializers
 from apps.booking.models import Booking
 from apps.trip.models import Trip
@@ -6,7 +7,7 @@ from apps.seat.serializers import SeatSerializer
 from apps.trip.serializers import TripDetailSerializer
 from apps.payment.serializers import PaymentSerializer
 
-
+logger = logging.getLogger(__name__)
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -29,36 +30,44 @@ class BookingDetailSerializer(serializers.ModelSerializer):
         read_only_fields = ['booking_datetime', 'user', 'total_price']
 
     def create(self, validated_data):
+        logger.debug("Creating new booking")
         trip_id = self.initial_data.get('trip_id')
-        seats_ids = self.initial_data.get('seats_ids', [])
+        seat_numbers = self.initial_data.get('seat_numbers', [])
 
         if not trip_id:
+            logger.error("Trip ID is required")
             raise serializers.ValidationError({"trip_id": "Необходимо указать ID поездки"})
 
-        if not seats_ids:
-            raise serializers.ValidationError({"seats_ids": "Необходимо выбрать хотя бы одно место"})
+        if not seat_numbers:
+            logger.error("seat numbers required")
+            raise serializers.ValidationError({"seat_numbers": "Необходимо выбрать хотя бы одно место"})
 
         trip = Trip.objects.get(pk=trip_id)
         validated_data['trip'] = trip
 
-        trip_seats = TripSeat.objects.filter(trip_id=trip_id, seat_id__in=seats_ids, is_booked=False)
+        trip_seats = TripSeat.objects.filter(trip_id=trip_id, seat__seat_number__in=seat_numbers, is_booked=False)
 
         # Проверяем, что все места доступны
-        if trip_seats.count() != len(seats_ids):
+        if trip_seats.count() != len(seat_numbers):
             # Находим недоступные места
             available_seat_ids = [ts.seat_id for ts in trip_seats]
-            unavailable_ids = [s_id for s_id in seats_ids if s_id not in available_seat_ids]
+            unavailable_ids = [s_id for s_id in seat_numbers if s_id not in available_seat_ids]
+            logger.error("Some seats are not available")
             raise serializers.ValidationError(
-                {"seats_ids": f"Места с ID {', '.join(map(str, unavailable_ids))} недоступны для бронирования"}
+                {"seat_numbers": f"Места с номером {', '.join(map(str, unavailable_ids))} недоступны для бронирования"}
             )
 
         # Создаем бронирование
         booking = Booking.objects.create(**validated_data)
+        logger.info(f"Created new booking: {booking}")
 
         # Бронируем места
+        logger.debug("Booking some seats")
         for trip_seat in trip_seats:
             trip_seat.is_booked = True
             trip_seat.save()
             booking.trip_seats.add(trip_seat)
+            
+        logger.info("Seats successfully booked")
 
         return booking

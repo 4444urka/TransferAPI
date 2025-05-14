f81e409c248a84f5ca822b1ad4b24b6979138866
4444urka <149653447+4444urka@users.noreply.github.com>
2025-05-14 17:22:52 +1000

    Добавлен пользовательский обработчик исключений для улучшения обработки ошибок в API

--- src/config/settings.py
+++ src/config/settings.py
@@ -159,6 +159,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+    'EXCEPTION_HANDLER': 'utils.exception_handlers.custom_exception_handler',
 }
 
 SIMPLE_JWT = {

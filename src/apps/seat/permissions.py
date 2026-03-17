8954640b5ccafe30cce814e3a0aaa28d7d27dd8a
4444urka <149653447+4444urka@users.noreply.github.com>
2026-03-17 15:13:06 +1000

    Поддержка rows и seats_per_row в Vehicle
    Добавлены свойства row_index/col_index для Seat и включены в
    сериализаторы.
    Обновлён VehicleAdmin и добавлены тесты для расчёта и валидации мест.
    В тестах бронирования патчится requests.get; docker-compose обновлён до
    postgres:17.
    Изменён User-Agent геокодера, ужесточена проверка прав и внесены мелкие
    правки форматирования

--- src/apps/seat/permissions.py
+++ src/apps/seat/permissions.py
@@ -10,13 +10,13 @@ class HasSeatPermission(permissions.BasePermission):
 
 
     def has_permission(self, request, view):
-        
+
         # Если суперчелик, то нуууу проходи
         if request.user.is_superuser:
             return True
 
         # Пусть смотрит
-        if view.action in ['list', 'retrieve', 'get_seats_by_vehicle']:
+        if view.action in ['list', 'retrieve', 'get_seats_by_vehicle'] and request.user.is_authenticated:
             return True
 
         # Если пользователь хочет создать место, то он должен иметь право на это
@@ -30,8 +30,3 @@ class HasSeatPermission(permissions.BasePermission):
 
         elif view.action == 'destroy' and request.user.has_perm('seat.can_delete_seat'):
             return True
-
-
-
-
-
